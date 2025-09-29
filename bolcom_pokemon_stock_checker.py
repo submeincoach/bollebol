6572eba2516f37a2f7468b1387a5f55a0b1861e4
submeincoach <trawin85@gmail.com>
2025-09-29 13:50:34 +0200

    Update bolcom_pokemon_stock_checker.py

--- bolcom_pokemon_stock_checker.py
+++ bolcom_pokemon_stock_checker.py
@@ -13,7 +13,10 @@ DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 
 def check_stock(product_url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Referer': 'https://www.bol.com/',
     }
     response = requests.get(product_url, headers=headers)
     if response.status_code != 200:
